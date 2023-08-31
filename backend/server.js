1bdd5c8441b95cb34bc3d074f6b7aef049538ca1
Kelli Libin <km.putnam2@gmail.com>
2023-08-31 16:31:26 -0600

    achieved pagination, pushed riders to object

--- backend/server.js
+++ backend/server.js
@@ -4,36 +4,62 @@ dotenv.config();
 import connectDB from "./config/db.js";
 
 import puppeteer from "puppeteer";
-const offset = [0, 100, 200, 300, 400, 500];
-const url = `https://www.procyclingstats.com/rankings.php?date=2023-08-29&nation=&age=&zage=&page=smallerorequal&team=&offset=0&teamlevel=&filter=Filter&p=me&s=uci-individual`;
 
-const scrapeRiders = async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-  await page.goto(url);
-  const riderData = await page.evaluate(() => {
-    const riderRows = Array.from(document.getElementsByTagName("tr"));
+const scrapeRiders = async (desiredOffset) => {
+  //check for current page, if not start at 0
+  try {
+    if (!desiredOffset) {
+      desiredOffset = 0;
+    }
+    //load browser and page
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
 
-    const riderInfo = riderRows.map((rider) => {
-      const tdElements = rider.querySelectorAll("td");
-      const spanElement = rider.querySelector("span.flag");
-      const aElements = rider.querySelectorAll("a");
+    //init variables that will be changed with each loop
+    let currentOffset = 0;
+    let riderData = [];
 
-      return {
-        ranking: tdElements[0]?.textContent.trim() || "",
-        riderName: aElements[0]?.textContent.trim() || "",
-        team: tdElements[4]?.textContent.trim() || "",
-        points: aElements[1]?.textContent.trim() || "",
-      };
-    });
+    //riders are limited to 100 per page. loop through and change url so that you can get multiple pages of riders
+    while (currentOffset <= desiredOffset) {
+      const url = `https://www.procyclingstats.com/rankings.php?date=2023-08-29&nation=&age=&zage=&page=smallerorequal&team=&offset=${currentOffset}&teamlevel=&filter=Filter&p=me&s=uci-individual`;
+      await page.goto(url);
 
-    return riderInfo;
-  });
-  console.log(riderData);
-  await browser.close();
+      //grab rider info from the tr elements
+      const pageRiderData = await page.evaluate(() => {
+        const riderRows = Array.from(document.getElementsByTagName("tr"));
+
+        //skip first row since it's the header
+        const dataRows = riderRows.slice(1);
+
+        const riderInfo = dataRows.map((rider) => {
+          //map through each tr and grab elements
+          const tdElements = rider.querySelectorAll("td");
+          const spanElement = rider.querySelectorAll("span");
+          const aElements = rider.querySelectorAll("a");
+
+          return {
+            ranking: tdElements[0].textContent.trim() || "",
+            nationality: spanElement[1].className,
+            riderName: aElements[0].textContent.trim() || "",
+            team: tdElements[4].textContent.trim() || "",
+            points: aElements[1].textContent.trim() || "",
+          };
+        });
+
+        return riderInfo;
+      });
+      //add riders to the array and increase the offset to trigger new page load
+      riderData.push(pageRiderData);
+      currentOffset += 100;
+    }
+    console.log(riderData);
+    await browser.close();
+  } catch (err) {
+    console.log(err);
+  }
 };
-
-scrapeRiders();
+//call
+scrapeRiders(100);
 
 const port = process.env.PORT || 5000;
 
