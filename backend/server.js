4aeb892cfe7409fffa0de3a24325dbbe8f5d0d94
Kelli Libin <km.putnam2@gmail.com>
2023-09-06 15:42:05 -0600

    implement cyclist routes

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,24 @@ dotenv.config();
 //database
 import connectDB from "./config/db.js";
 
+//routes
+import cyclistRoutes from './routes/cyclistRoutes.js'
+
+//port variable
 const port = process.env.PORT || 5000;
 
 //init db and server
 connectDB();
 const app = express();
 
+//body parser middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is up and running...");
 });
 
-app.get("/api/riders", (req, res) => {
-  res.send("riders");
-});
-
-app.get("/api/races", (req, res) => {
-  res.send("races");
-});
+//routes
+app.use('/api/cyclists', cyclistRoutes)
 
 app.listen(port, () => console.log(`server running on port ${port}`));
