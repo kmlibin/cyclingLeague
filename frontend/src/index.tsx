08da15da55a003d5aebce9ef2bed7c156aaedee3
Kelli Libin <km.putnam2@gmail.com>
2023-09-06 15:43:26 -0600

    added routes, wrapped in rtk provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/index.css";
+import App from "./App";
+import Roster from "./screens/Roster";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Roster />} />
+    </Route>
+  )
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
