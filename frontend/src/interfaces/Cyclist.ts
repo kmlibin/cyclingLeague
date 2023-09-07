11caa1a4e04c392019299ab6303844b0ab7362b7
Kelli Libin <km.putnam2@gmail.com>
2023-09-07 12:28:54 -0600

    added categories to cyclist interface

--- frontend/src/interfaces/Cyclist.ts
+++ frontend/src/interfaces/Cyclist.ts
@@ -1,13 +1,16 @@
 export interface Cyclist {
     _id: string;
     name: string;
+    yearEndRanking: number
     imageSrc: string;
     nationality: string;
     nationalityName: string;
+    socialUrls: [{icon: string, href: string}]
     riderSpecialties: [ {
         specialty: string;
         points: number;
     }],
+    mainSpecialty: string,
     team: string,
     yearEndUciPoints: number;
     currentUciPoints?: number
