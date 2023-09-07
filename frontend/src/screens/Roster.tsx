c0e49a4df11bca87f67e1160bf456c5ce035dee5
Kelli Libin <km.putnam2@gmail.com>
2023-09-07 12:29:19 -0600

    rendered table with cyclist data hid some styles

--- frontend/src/screens/Roster.tsx
+++ frontend/src/screens/Roster.tsx
@@ -1,22 +1,96 @@
-import React from 'react'
-import { useGetCyclistsQuery } from '../slices/cyclistApiSlice'
-import { Cyclist } from '../interfaces/Cyclist'
+import React from "react";
+import { useGetCyclistsQuery } from "../slices/cyclistApiSlice";
+import { Cyclist } from "../interfaces/Cyclist";
+import styled from 'styled-components'
+import  TabsComponent  from '../components/TabsComponent'
 
-type Props = {}
+import DataTable, { TableColumn } from "react-data-table-component";
+
+type DataRow = {
+  // rank: number;
+  mainSpecialty: string;
+  name: string;
+  team: string;
+  yearEndUciPoints: number;
+  cost: number;
+};
+
+const columns: TableColumn<DataRow>[] = [
+  // {
+  //   name: "Rank",
+  //   selector: (row) => row.rank,
+  // },
+  {
+    name: "Specialty",
+    selector: (row) => row.mainSpecialty,
+  },
+  {
+    name: "Name",
+    selector: (row) => row.name,
+  },
+  {
+    name: "Team",
+    selector: (row) => row.team,
+  },
+  {
+    name: "Points",
+    selector: (row) => row.yearEndUciPoints,
+  },
+  {
+    name: "Cost",
+    selector: (row) => row.cost,
+  },
+];
+
+
+
+type Props = {};
+
+//override some of the styling of react data table
+const HideSelectionSummary = styled.div`
+  .sc-ksJisA.da-DRew {
+    display: none;
+  }
+  .goRgCU {
+   z-index: -10
+  }
+  .sc-hLclGa.sc-bqOBKd.eWcvIQ.kNUNAY.rdt_TableCol input[type="checkbox"] {
+    display: none;
+  }
+`;
+
 
 const Roster = (props: Props) => {
-
   const { data: cyclists } = useGetCyclistsQuery({});
-  console.log(cyclists)
+  const handleChange = ({ selectedRows }: { selectedRows: DataRow[] }) => {
+    // You can set state or dispatch with something like Redux so we can use the retrieved data
+    console.log("Selected Rows: ");
+    
+  };
   return (
     <>
-      {cyclists.map((cyclist: Cyclist) => (
-        <div key={cyclist._id}>
-          {cyclist.name} {/* Display cyclist data */}
-        </div>
-      ))}
+    <TabsComponent />
+    <HideSelectionSummary>
+      <DataTable
+        title="League Roster"
+        columns={columns}
+        data={cyclists || []}
+        selectableRows
+        onSelectedRowsChange={handleChange}
+        dense
+        pagination
+        paginationPerPage={20}
+      />
+      </HideSelectionSummary>
     </>
   );
-}
+};
 
-export default Roster
+export default Roster;
+//  {cyclists?.map((cyclist: Cyclist) => (
+//     <div key={cyclist._id}>
+//       {cyclist.name} {/* Display cyclist data */}
+//     </div>
+//   ))}
+
+
