9e68804584956dcf7163d7ddc2ecfcfd88b85bba
Kelli Libin <km.putnam2@gmail.com>
2023-09-08 11:47:29 -0600

    create

---
+++ frontend/src/screens/CyclistScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Cyclist } from "../interfaces/Cyclist";
+
+type Props = {
+
+}
+const CyclistScreen: React.FC<Props> = () => {
+
+
+  return (
+    <div>React.FC</div>
+  )
+}
+
+export default CyclistScreen
