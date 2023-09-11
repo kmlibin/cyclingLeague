01d8ef8e3ccc40066575f32222422d9e62ab54c2
Kelli Libin <km.putnam2@gmail.com>
2023-09-11 16:17:25 -0600

    quick check to make sure i can get data

--- frontend/src/screens/TeamScreen.tsx
+++ frontend/src/screens/TeamScreen.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useGetSingleTeamQuery, useGetTeamsQuery } from '../slices/cyclistApiSlice';
 
 const TeamScreen = () => {
+    const { name } = useParams();
+    const {data } = useGetTeamsQuery({})
+  //need to decode the names...right now, it's passed as tadej%20Pogacar because spaces are dumb. but i'd rather use rider name than id
+  const decodedName = decodeURIComponent(name || "");
+    
+    const { data: team } = useGetSingleTeamQuery(decodedName);
+    console.log(team)
+    console.log(data)
   return (
     <div>TeamScreen</div>
   )
