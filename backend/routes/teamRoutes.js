dcc55963060fce16e9555fc6ae3fc73fb175f024
Kelli Libin <km.putnam2@gmail.com>
2023-09-11 16:11:50 -0600

    routes for get all and get single

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 import {
-  getTeams
+  getAllTeams, getSingleTeam
 } from "../controllers/controllerList.js";
 
 const router = express.Router();
 
-router.route("/").get(getTeams);
+router.route("/").get(getAllTeams);
+router.route("/:name").get(getSingleTeam)
 
 export default router;
