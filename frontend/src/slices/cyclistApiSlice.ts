afd5d8177b95969809a6c8b7163ebaebcd099e94
Kelli Libin <km.putnam2@gmail.com>
2023-09-11 16:12:30 -0600

    queries for get all and single team

--- frontend/src/slices/cyclistApiSlice.ts
+++ frontend/src/slices/cyclistApiSlice.ts
@@ -1,25 +1,39 @@
-import { CYCLISTS_URL } from "../constants";
+import { CYCLISTS_URL, TEAMS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const cyclistsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getCyclists: builder.query({
-      query: ({tab, keyword}) => ({
+      query: ({ tab, keyword }) => ({
         url: CYCLISTS_URL,
         method: "GET",
-        params: {tab, keyword},
+        params: { tab, keyword },
       }),
       providesTags: ["Cyclist"],
-      keepUnusedDataFor: 5
+      keepUnusedDataFor: 5,
     }),
     getSingleCyclist: builder.query({
       query: (cyclistName) => ({
-        url: `${CYCLISTS_URL}/${cyclistName}`
+        url: `${CYCLISTS_URL}/${cyclistName}`,
       }),
-      keepUnusedDataFor: 2
-    })
+      keepUnusedDataFor: 2,
+    }),
+    getTeams: builder.query({
+      query: () => ({
+        url: TEAMS_URL,
+        method: "GET",
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getSingleTeam: builder.query({
+      query: (teamName) => ({
+        url: `${TEAMS_URL}/${teamName}`,
+        method: "GET",
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
- 
 });
 
-export const { useGetCyclistsQuery, useGetSingleCyclistQuery } = cyclistsApiSlice;
+export const { useGetCyclistsQuery, useGetSingleCyclistQuery, useGetTeamsQuery } =
+  cyclistsApiSlice;
