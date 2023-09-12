d5511e48de99937fd0cb6fa595b9ddc035011af0
Kelli Libin <km.putnam2@gmail.com>
2023-09-12 10:20:28 -0600

    render nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
+    <NavBar />
       <main className="py-3">
         <Container>
           <Outlet />
