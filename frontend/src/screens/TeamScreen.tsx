4e8fbf7bc0b316f0860df532d3fd84a92b2dafc1
Kelli Libin <km.putnam2@gmail.com>
2023-09-12 14:20:56 -0600

    map through and show cyclist data components

--- frontend/src/screens/TeamScreen.tsx
+++ frontend/src/screens/TeamScreen.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useGetSingleTeamQuery, useGetTeamsQuery } from '../slices/cyclistApiSlice';
+import CyclistData from '../components/CyclistData';
+import { Cyclist } from '../interfaces/Cyclist';
 
 const TeamScreen = () => {
     const { name } = useParams();
-    const {data } = useGetTeamsQuery({})
   //need to decode the names...right now, it's passed as tadej%20Pogacar because spaces are dumb. but i'd rather use rider name than id
   const decodedName = decodeURIComponent(name || "");
     
     const { data: team } = useGetSingleTeamQuery(decodedName);
-    console.log(team)
-    console.log(data)
+    console.log(team?.cyclists)
+  
   return (
-    <div>TeamScreen</div>
+    <>
+    {team?.cyclists?.map((rider: Cyclist) => (
+        <CyclistData key={rider._id} cyclistData={rider} /> // Return the component here
+    ))}
+</>
   )
 }
+
 
 export default TeamScreen
