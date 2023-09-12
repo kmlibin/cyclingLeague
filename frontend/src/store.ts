149c0bbe0a84167e3ec1df6c14e8d535e30d78f7
Kelli Libin <km.putnam2@gmail.com>
2023-09-12 10:22:08 -0600

    add auth to store, access logout and userinfo

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
-
+import authSliceReducer from './slices/authSlice'
+ 
 const store = configureStore({
   reducer: {
     //don't have to bring other api reducers in because we are using apiSlice.inject endpoints
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
