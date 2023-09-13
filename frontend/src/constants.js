8fe622e43e3dfb1f505f094e0c7f09b86051f360
Kelli Libin <km.putnam2@gmail.com>
2023-09-13 17:45:12 -0600

    added league url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
 export const BASE_URL = "";
 export const CYCLISTS_URL = "/api/cyclists";
 export const TEAMS_URL = "/api/teams";
-export const USERS_URL = "/api/users"
+export const USERS_URL = "/api/users";
+export const LEAGUE_URL = "/api/leagues";
