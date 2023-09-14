1f3f2dda2abcc5511f59ece25f80420286d1c3d5
Kelli Libin <km.putnam2@gmail.com>
2023-09-14 12:46:09 -0600

    add route for getting single team

--- backend/routes/fantasyTeamRoutes.js
+++ backend/routes/fantasyTeamRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 
 import {
-  createTeam
+  createTeam, getSingleFantasyTeam
 } from "../controllers/fantasyTeamController.js";
 
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
-router.route("/").post(protect, createTeam);
+router.route("/").post(protect, createTeam).get(protect, getSingleFantasyTeam);
 
 
 export default router;
