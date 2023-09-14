0451940a8b55543f5d7f576699a651ff8734fceb
Kelli Libin <km.putnam2@gmail.com>
2023-09-14 12:45:36 -0600

    added owner and team name to the created team

--- backend/controllers/fantasyTeamController.js
+++ backend/controllers/fantasyTeamController.js
@@ -1,26 +1,44 @@
-import FantasyTeam from '../models/FantasyTeam.js';
-import {StatusCodes} from 'http-status-codes'
-
-
+import FantasyTeam from "../models/FantasyTeam.js";
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
 
 //@desc create league
-//@route POST /api/leagues
+//@route POST /api/fantasyteam
 //@access Public
 const createTeam = async (req, res) => {
-    const { cyclistIds} = req.body
-    console.log(req)
+  const { cyclistIds, teamName } = req.body;
+  const owner = req.user._id;
 
-    const team = new FantasyTeam({
-        cyclists: cyclistIds
-    })
-    const createdTeam = await team.save()
-    console.log(createdTeam)
-    res.status(StatusCodes.CREATED).json({createdTeam})
-}
+  //if team is not 25 ids, throw error
+
+  const team = new FantasyTeam({
+    owner,
+    cyclists: cyclistIds,
+    teamName,
+  });
+  const createdTeam = await team.save();
+
+  // update the user's myTeam field
+  await User.findByIdAndUpdate(owner, { myTeam: createdTeam._id });
+  console.log(createdTeam);
+  res.status(StatusCodes.CREATED).json({ createdTeam });
+};
 
 //@desc fetch all leagues
-//@route GET /api/leagues
+//@route GET /api/fantasyteam
 //@access Public
+
+const getSingleFantasyTeam = async (req, res) => {
+  const { _id: teamId } = req.body;
+  console.log(teamId);
+  const fantasyTeam = await FantasyTeam.findById(teamId).populate("cyclists");
+  if (!fantasyTeam) {
+    res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: "no cyclist with that name" });
+  }
+  res.status(StatusCodes.OK).json(fantasyTeam);
+};
 // const getAllLeagues = async(req,res) => {
 //     try {
 //         const leagues = await League.find().populate('cyclists')
@@ -31,4 +49,4 @@ const createTeam = async (req, res) => {
 //     }
 // }
 
-export {createTeam}
+export { createTeam, getSingleFantasyTeam };
