f569c4e1999f0f7dcea2c02c58a17e29823eed64
Kelli Libin <km.putnam2@gmail.com>
2023-09-14 09:59:23 -0600

    added cookie parser

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ import express from "express";
 //libraries
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 
 //database
 import connectDB from "./config/db.js";
 
 //routes
 import cyclistRoutes from "./routes/cyclistRoutes.js";
-import teamRoutes from './routes/teamRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import fantasyTeamRoutes from './routes/fantasyTeamRoutes.js'
+import teamRoutes from "./routes/teamRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import fantasyTeamRoutes from "./routes/fantasyTeamRoutes.js";
 
 import createTeamData from "./utils/aggregations.js";
 
@@ -23,29 +24,25 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-
-
 //body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
-//create team data through aggregation 
+//create team data through aggregation
 // createTeamData();
 
 app.get("/", (req, res) => {
   res.send("API is up and running...");
 });
 
-
 //routes
 app.use("/api/cyclists", cyclistRoutes);
-app.use("/api/teams", teamRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/fantasyteam", fantasyTeamRoutes)
+app.use("/api/teams", teamRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/fantasyteam", fantasyTeamRoutes);
 
 try {
   // await createTeamData()
   app.listen(port, () => console.log(`server running on port ${port}`));
-} catch (error) {
-  
-}
-
+} catch (error) {}
