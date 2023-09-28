5fdd69cf5156f4ccad46eb0fd17ce60f0d9376e9
Kelli Libin <km.putnam2@gmail.com>
2023-09-28 12:46:27 -0600

    set up general template

--- frontend/src/screens/DashboardScreen.tsx
+++ frontend/src/screens/DashboardScreen.tsx
@@ -1,18 +1,46 @@
 import React from "react";
+import { useAppSelector } from "../hooks/hooks";
 import { useGetSingleFantasyTeamQuery } from "../slices/fantasyTeamApiSlice";
 import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-import CyclistData from "../components/CyclistData";
+
+import TeamList from "../components/TeamList";
 
 const DashboardScreen: React.FC = () => {
-    const {id} = useParams()
-    console.log(id)
-    const { data: team } = useGetSingleFantasyTeamQuery(id);
-    console.log(team);
-
+  const { id } = useParams();
+  console.log(id);
+  const { data: team } = useGetSingleFantasyTeamQuery(id);
+  console.log(team);
+  const { userInfo } = useAppSelector((state) => state.auth);
+  console.log(userInfo);
   return (
-    <p>DashbArod screen</p>
-  )
+    <Container fluid className="d-flex flex-row justify-content-center">
+      <Row style ={{backgroundColor: 'yellow'}} xs={1} md={3}>
+        <Col>
+          <p>gots to say a lot of stuff</p>
+        </Col>
+      </Row>
+      <Row  xs={1} md={9} style ={{backgroundColor: 'pink'}}>
+        <Col>
+          <Container fluid className="d-flex flex-column"  >
+            <Row  style ={{backgroundColor: 'blue'}}>
+              <Col>
+                <TeamList data={team}/>
+              </Col>
+            </Row>
+            <Row  style ={{backgroundColor: 'orange'}}>
+              <Col>
+                <p>adding another something</p>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default DashboardScreen;
