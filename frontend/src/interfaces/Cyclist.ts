891781b01ca7c7966b209faa8e7e0aeb786bf47d
Kelli Libin <km.putnam2@gmail.com>
2023-09-29 13:48:33 -0600

    added user and fantasy team. needs split or rename

--- frontend/src/interfaces/Cyclist.ts
+++ frontend/src/interfaces/Cyclist.ts
@@ -1,24 +1,37 @@
 export interface Cyclist {
-    _id: string;
-    name: string;
-    yearEndRanking: number;
-    imageSrc: string;
-    nationality: string;
-    nationalityName: string;
-    socialUrls: [{icon: string, href: string}]
-    riderSpecialties: [ {
-        specialty: string;
-        points: number;
-    }],
-    mainSpecialty: string,
-    team: string,
-    yearEndUciPoints: number;
-    currentUciPoints?: number
-  }
+  _id: string;
+  name: string;
+  yearEndRanking: number;
+  imageSrc: string;
+  nationality: string;
+  nationalityName: string;
+  socialUrls: [{ icon: string; href: string }];
+  riderSpecialties: [
+    {
+      specialty: string;
+      points: number;
+    }
+  ];
+  mainSpecialty: string;
+  team: string;
+  yearEndUciPoints: number;
+  currentUciPoints?: number;
+}
 
-  export interface FantasyTeam {
-    _id: string;
+export interface FantasyTeam {
+  _id: string;
+  teamName: string;
+  owner: string;
+  cyclists: Cyclist[];
+}
+
+export interface User {
+  name: string;
+  email: string;
+  _id: string;
+  isAdmin: boolean;
+  fantasyTeam: {
+    cyclists: string[];
     teamName: string;
-    owner: string;
-    cyclists: Cyclist[];
-  }
+  };
+}
