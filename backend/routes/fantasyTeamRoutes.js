c0f0a6e3d6b6b139bdd8fda9add66c25aff8e288
Kelli Libin <km.putnam2@gmail.com>
2023-10-02 14:14:48 -0600

    added get single team by team id to routes

--- backend/routes/fantasyTeamRoutes.js
+++ backend/routes/fantasyTeamRoutes.js
@@ -1,19 +1,22 @@
 import express from "express";
 
 import {
-  createTeam, getSingleFantasyTeam, getAllFantasyTeams, createLeague
+  createTeam,
+  getSingleFantasyTeam,
+  getAllFantasyTeams,
+  createLeague,
+  getSingleFantasyTeamById,
 } from "../controllers/fantasyTeamController.js";
 
-import { protect } from '../middleware/authMiddleware.js'
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(protect, createTeam).get(getAllFantasyTeams)
+router.route("/").post(protect, createTeam).get(getAllFantasyTeams);
+router.route("/teams/:teamId").get(getSingleFantasyTeamById);
 router.route("/:userId").get(protect, getSingleFantasyTeam);
-router.route("/:userId/myleague").patch(protect, createLeague)
-
+router.route("/:userId/myleague").patch(protect, createLeague);
 
 export default router;
-
 
 //@route PATCH /api/fantasyteam/:id/myleague
