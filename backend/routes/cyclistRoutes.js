561fc8e3f3bbced2f90fa103296ab69af575be61
Kelli Libin <km.putnam2@gmail.com>
2023-10-04 16:32:45 -0600

    format

--- backend/routes/cyclistRoutes.js
+++ backend/routes/cyclistRoutes.js
@@ -7,7 +7,7 @@ import {
 
 const router = express.Router();
 
-router.route("/").get(getAllRiders);
+router.route("/").get( getAllRiders);
 router.route("/:name").get(getSingleRider);
 
 export default router;
