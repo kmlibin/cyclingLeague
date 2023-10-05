41e6d31034dc88bb8dd7d1b0c42fa8ea6e31c84b
Kelli Libin <km.putnam2@gmail.com>
2023-10-05 11:47:07 -0600

    updated imports/exports

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,5 +1,25 @@
-import { scrapeTable, scrapeURLS, scrapeAndUpdateRank, seedDataBase, updateYearEndRank, updatePointsAndRank } from './scraping.js'
-import { createTeamData } from './aggregations.js';
+import {
+  scrapeTable,
+  scrapeURLS,
+  scrapeAndUpdateRank,
+  seedDataBase,
+  updateYearEndRank,
+  updatePointsAndRank,
+} from "./scraping.js";
+import { createTeamData } from "./aggregations.js";
 import generateToken from "./generateToken.js";
+import { calculateTotalPoints } from "./calculateTotalPoints.js";
+import { recalculateTotalPoints } from "./recalculateTotalPoints.js";
 
-export { scrapeTable, scrapeURLS, scrapeAndUpdateRank, seedDataBase, updateYearEndRank, generateToken, createTeamData, updatePointsAndRank };
+export {
+  scrapeTable,
+  scrapeURLS,
+  scrapeAndUpdateRank,
+  seedDataBase,
+  updateYearEndRank,
+  generateToken,
+  createTeamData,
+  updatePointsAndRank,
+  calculateTotalPoints,
+  recalculateTotalPoints,
+};
