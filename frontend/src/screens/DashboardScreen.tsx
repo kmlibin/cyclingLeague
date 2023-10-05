7d3f14836404866858ad60f20fd56e7be9627afc
Kelli Libin <km.putnam2@gmail.com>
2023-10-05 13:13:44 -0600

    display league in a table, better padding/margins

--- frontend/src/screens/DashboardScreen.tsx
+++ frontend/src/screens/DashboardScreen.tsx
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Table from "react-bootstrap/Table";
 
 import TeamList from "../components/TeamList";
 
@@ -16,7 +17,7 @@ const DashboardScreen: React.FC = () => {
   console.log(id);
   const { data: team } = useGetSingleFantasyTeamQuery(id);
   const { data: league, refetch }: any = useGetLeagueQuery(id);
-  // console.log(league);
+  console.log(league);
   const { userInfo } = useAppSelector((state) => state.auth);
 
   //can't figure out why, but getleaguequery isn't hitting the backend on navigate, only does so if i refresh the page. this forces it
@@ -25,7 +26,6 @@ const DashboardScreen: React.FC = () => {
     refetch();
   }, []);
 
-  console.log(userInfo);
   return (
     <Container
       fluid
@@ -38,27 +38,41 @@ const DashboardScreen: React.FC = () => {
         </Col>
       </Row>
       <Row xs={1} md={9} style={{ backgroundColor: "pink" }}>
+      <Row className="mt-4 mb-4 text-center">
+              <h2>
+                <b>{team?.teamName}</b>
+              </h2>
+            </Row>
         <Col>
           <Container fluid className="d-flex flex-column">
             <Row style={{ backgroundColor: "blue" }}>
-              <Col>
+              <Col className="mt-4">
                 <TeamList data={team} />
               </Col>
             </Row>
-
-            <Row
-              className="d-flex flex-column"
-              style={{ backgroundColor: "orange" }}
-            >
-              <Col>
-                <h2>{league?.name}</h2>
-              </Col>
-              <Col>
-                {league?.teamIds.map((team: any) => {
-                  return <div key={team._id}>{team.teamName}</div>;
-                })}
-              </Col>
+            <Row className="mt-4 mb-4 text-center">
+              <h2>
+                My League: <b>{league?.name}</b>
+              </h2>
             </Row>
+            <Container style={{ maxWidth: "60%" }}>
+              <Table striped bordered hover responsive>
+                <thead>
+                  <tr>
+                    <th>Team Name</th>
+                    <th className="d-flex justify-content-end">Score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {league?.teamIds.map((team: any) => (
+                    <tr key={team._id}>
+                      <td>{team.teamName}</td>
+                      <td className="d-flex justify-content-end">{team.totalPoints}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Container>
           </Container>
         </Col>
       </Row>
