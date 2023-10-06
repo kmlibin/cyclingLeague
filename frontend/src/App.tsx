fddcbcae706b2c2bea562c25b4511724bb54686e
Kelli Libin <km.putnam2@gmail.com>
2023-10-06 15:56:45 -0600

    delete import of unused css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css'
 import NavBar from "./components/NavBar";
+
 
 const App = () => {
   return (
