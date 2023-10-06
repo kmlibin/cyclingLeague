701bf41d2bca7fcfcb3e0758d808c65b9f71ec37
Kelli Libin <km.putnam2@gmail.com>
2023-10-06 15:53:56 -0600

    adde nationalilty and rankrev

---
+++ frontend/src/types/DataRow.ts
@@ -0,0 +1,11 @@
+export type DataRow = {
+    // rank: number;
+    mainSpecialty: string;
+    name: string;
+    team: string;
+    yearEndUciPoints: number;
+    price: number;
+    _id: string;
+    nationalityName: string;
+    prevYearRank: string;
+  };
