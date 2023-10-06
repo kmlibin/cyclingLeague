4408467fbf67ca9bd3add9f9295abd6287661be5
Kelli Libin <km.putnam2@gmail.com>
2023-10-06 15:54:16 -0600

    show team name, wrapped in container

--- frontend/src/screens/TeamDataScreen.tsx
+++ frontend/src/screens/TeamDataScreen.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
-import { useGetSingleFantasyTeamByIdQuery } from '../slices/fantasyTeamApiSlice';
-import { useGetSingleTeamQuery } from '../slices/cyclistApiSlice';
-import CyclistData from '../components/CyclistData';
-import { Cyclist } from '../interfaces/Cyclist';
+import { useGetSingleFantasyTeamByIdQuery } from "../slices/fantasyTeamApiSlice";
+import { useGetSingleTeamQuery } from "../slices/cyclistApiSlice";
+import CyclistData from "../components/CyclistData";
+import { Cyclist } from "../interfaces/Cyclist";
 
 const TeamDataScreen: React.FC = () => {
-    const { teamId } = useParams();
-    const  {name} = useParams();
+  const { teamId } = useParams();
+  const { name } = useParams();
 
-  const { data: fantasyTeam } = useGetSingleFantasyTeamByIdQuery(teamId || '');
-  const { data: team } = useGetSingleTeamQuery(decodeURIComponent(name || ''));
+  const { data: fantasyTeam } = useGetSingleFantasyTeamByIdQuery(teamId || "");
+  const { data: team } = useGetSingleTeamQuery(decodeURIComponent(name || ""));
 
   const cyclists = fantasyTeam?.cyclists || team?.cyclists || [];
 
   return (
     <>
-      {cyclists.map((rider: Cyclist) => (
-        <CyclistData key={rider._id} cyclistData={rider} />
-      ))}
+      {team && <h1 className="text-center mt-2 mb-5">{team._id}</h1>}
+      {fantasyTeam && <h1 className="text-center mt-2 mb-5">{fantasyTeam.teamName}</h1>}
+      <Container
+        fluid
+        className="d-flex flex-wrap justify-content-evenly"
+        style={{ backgroundColor: "pink" }}
+      >
+        {cyclists.map((rider: Cyclist) => (
+          <CyclistData key={rider._id} cyclistData={rider} />
+        ))}
+      </Container>
     </>
   );
 };
