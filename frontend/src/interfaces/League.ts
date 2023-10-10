f502c23d338d69645f1d987bc1fd76974337a7e5
Kelli Libin <km.putnam2@gmail.com>
2023-10-10 13:18:41 -0600

    interface for league data

---
+++ frontend/src/interfaces/League.ts
@@ -0,0 +1,16 @@
+import { Cyclist } from "./Cyclist"
+
+export interface LeagueData {
+    name: string,
+    teamIds: {
+      cyclists: Cyclist[],
+      owner: {
+        _id: string,
+        name: string,
+      },
+      teamName: string,
+      totalPoints: number | string,
+      _id: string
+    },
+    
+  }
