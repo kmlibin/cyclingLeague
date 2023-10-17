bf3d34251e9daffc7dc92ed401f66e3b11375635
Kelli Libin <km.putnam2@gmail.com>
2023-10-17 17:28:36 -0600

    split into own file

---
+++ frontend/src/types/TeamError.ts
@@ -0,0 +1,7 @@
+export type TeamError = {
+    alreadyOnTeam?: string | undefined;
+    teamLength?: string | undefined;
+    teamName?: string | undefined;
+    pointsUsed?: string | undefined;
+    serverError?: string | undefined;
+  };
