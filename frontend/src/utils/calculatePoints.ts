2f1c8cb6acf7be73a8cf3521f8db73eb0aa712a9
Kelli Libin <km.putnam2@gmail.com>
2023-10-17 17:25:09 -0600

    delete and renamed

--- frontend/src/utils/calculatePoints.ts
+++
@@ -1,9 +0,0 @@
-
-const calculatePrice = (prevPoints: number) => {
-    const psRatio = 0.00816;
-    //round to the nearest number, but will always at least be 1
-    const price = Math.max(1, Math.round(prevPoints * psRatio));
-    return price;
-  };
-
-  export default calculatePrice
