e17d1f8c0a6989b6e023130284961dfa29199dee
Kelli Libin <km.putnam2@gmail.com>
2023-10-26 15:47:54 -0600

    grab userinfo, display dashboard btn if logged in

--- frontend/src/screens/LandingScreen.tsx
+++ frontend/src/screens/LandingScreen.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+//redux
+import { useAppSelector } from "../hooks/hooks";
+
+//styles
 import "./LandingScreen.css";
+
+//bootstrap and libraries
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,6 +16,11 @@ import { LinkContainer } from "react-router-bootstrap";
 import { MdOutlineDirectionsBike } from "react-icons/md";
 
 const LandingScreen = () => {
+  const { userInfo } = useAppSelector((state) => state.auth);
+  //grab id from state so can navigate to dashboard page, and conditionally
+  //render login/register ui
+  const id = typeof userInfo === "object" ? userInfo._id : null;
+
   return (
     <Container fluid>
       <Row>
@@ -20,6 +32,13 @@ const LandingScreen = () => {
           md={5}
           className="text-content d-flex flex-column align-items-end justify-content-center"
         >
+          {userInfo && (
+            <div className="dashboard">
+              <LinkContainer to={`/users/${id}/dashboard`}>
+                <Button className="button-style">My Dashboard</Button>
+              </LinkContainer>
+            </div>
+          )}
           <h1 className="text-color">
             The Best Cyclists. <br></br>On Your Team.
           </h1>
@@ -27,28 +46,34 @@ const LandingScreen = () => {
             <p className="text-end">
               Unlock the thrill of fantasy cycling by joining us today! Dive
               into a world of excitement as you handpick your dream team from a
-              pool of 900 top cycling professionals. Ready to take it up a notch? Create your
-              very own league and invite fellow fantasy teams to battle it out
-              for the ultimate cycling supremacy! Are you up for the challenge?
+              pool of 900 top cycling professionals. Ready to take it up a
+              notch? Create your very own league and invite fellow fantasy teams
+              to battle it out for the ultimate cycling supremacy! Are you up
+              for the challenge?
             </p>
-            <LinkContainer className="w-50" to="/register">
-              <Button className="button-style" size="lg">
-                Sign Up Now
-              </Button>
-            </LinkContainer>
+            {!userInfo && (
+              <LinkContainer className="w-50" to="/register">
+                <Button className="button-style" size="lg">
+                  Sign Up Now
+                </Button>
+              </LinkContainer>
+            )}
           </div>
-          <p className="bottom">
-            Already have an account? Login{" "}
-            <Link
-              to="/login"
-              style={{ color: "rgb(14, 121, 213)", textDecoration: "none" }}
-            >
-              here
-            </Link>
-            <MdOutlineDirectionsBike
-              style={{ color: "rgb(114, 216, 5)", marginLeft: ".5rem" }}
-            />
-          </p>
+
+          {!userInfo && (
+            <p className="bottom">
+              Already have an account? Login{" "}
+              <Link
+                to="/login"
+                style={{ color: "rgb(14, 121, 213)", textDecoration: "none" }}
+              >
+                here
+              </Link>
+              <MdOutlineDirectionsBike
+                style={{ color: "rgb(114, 216, 5)", marginLeft: ".5rem" }}
+              />
+            </p>
+          )}
         </Col>
       </Row>
     </Container>
