5785768de5e44d3bf99b6e6ac164982a2cf632c5
Kelli Libin <km.putnam2@gmail.com>
2023-10-26 15:51:23 -0600

    protect fantasy team route

--- backend/routes/fantasyTeamRoutes.js
+++ backend/routes/fantasyTeamRoutes.js
@@ -14,7 +14,7 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(protect, createTeam).get(getAllFantasyTeams);
-router.route("/teams/:teamId").get(getSingleFantasyTeamById);
+router.route("/teams/:teamId").get(protect, getSingleFantasyTeamById);
 router.route("/:userId").get(protect, getSingleFantasyTeam);
 router.route("/:userId/myleague").patch(protect, createLeague).get(protect, getLeague);
 
