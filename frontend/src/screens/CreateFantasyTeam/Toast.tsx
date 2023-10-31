bda7a4e0ba053ed127231d6055fbcb755bba3c6f
Kelli Libin <km.putnam2@gmail.com>
2023-10-31 16:13:04 -0600

    finished building ui, longer settimeout

--- frontend/src/screens/CreateFantasyTeam/Toast.tsx
+++ frontend/src/screens/CreateFantasyTeam/Toast.tsx
@@ -1,34 +1,41 @@
 import React, { useState, useEffect } from "react";
+
 import { ToastMessage } from "../../types/ToastMessage";
+import {IoCloseSharp} from 'react-icons/io5'
+
+import './Toast.css'
 
 type ToastProps = {
   message: ToastMessage | undefined;
-  onClose: () => void;
 };
-const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
+
+const Toast: React.FC<ToastProps> = ({ message }) => {
   const [visible, setVisible] = useState(false);
-  console.log(visible);
+
   useEffect(() => {
     setVisible(!!message);
 
     if (message) {
       const timer = setTimeout(() => {
         setVisible(false);
-      }, 3000);
+      }, 20000);
       return () => clearTimeout(timer);
     }
   }, [message]);
 
   return (
     <div className={`custom-toast ${visible ? "show" : ""}`}>
+      <button className="btn-flex" onClick={() => {setVisible(false)}}><IoCloseSharp style={{color: "white"}}/></button>
+      <div className="toast-content">
       <p>
         <span className="bold">Points Remaining: &nbsp;</span>
         {message?.points}
       </p>
       <p>
         <span className="bold">Roster: &nbsp;</span>
-        {message?.roster}
+        {message?.roster} / 25
       </p>
+      </div>
     </div>
   );
 };
