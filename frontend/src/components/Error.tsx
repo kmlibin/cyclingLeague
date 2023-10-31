09f1b36fceac161b29f50279215a69234a5f6eb6
Kelli Libin <km.putnam2@gmail.com>
2023-10-31 09:29:29 -0600

    moved styles into css file

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,16 +1,13 @@
 import React from "react";
+
 //interfaces and types
 import { ServerError } from "../interfaces/ServerError";
 
 type ErrorProps = {
-  error: ServerError
+  error: ServerError;
 };
 const Error: React.FC<ErrorProps> = ({ error }) => {
-  return (
-    <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
-      {error?.data.msg}
-    </div>
-  );
+  return <div className="error">{error?.data.msg}</div>;
 };
 
 export default Error;
