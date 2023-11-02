c3f68958320431a688dc6abdb097a5c312ca1275
Kelli Libin <km.putnam2@gmail.com>
2023-11-02 11:06:52 -0600

    script and code for production

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,10 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("API is up and running...");
-});
 
 //routes
 app.use("/api/cyclists", cyclistRoutes);
@@ -38,8 +33,22 @@ app.use("/api/teams", teamRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/fantasyteam", fantasyTeamRoutes);
 
+
+//for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is up and running...");
+  });
+}
+
 try {
   app.listen(port, () => console.log(`server running on port ${port}`));
 } catch (error) {
-  console.log(error)
+  console.log(error);
 }
